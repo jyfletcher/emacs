f7122e248f38a3d81004ff1646fba290b01a5bf4
dkJuYaFl <justin.yates.fletcher@lego.com>
2019-03-01 13:42:54 +0100

    Adding window resizing

--- init.el
+++ init.el
@@ -35,6 +35,14 @@
   (forward-line -1)
   (indent-according-to-mode))
 
+;; Window resize
+;; https://www.emacswiki.org/emacs/WindowResize
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+
 ;; Package repo configuration
 ;; Standard package.el + MELPA setup
 ;; (See also: https://github.com/milkypostman/melpa#usage)
