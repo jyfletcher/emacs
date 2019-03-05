69883dc0bcecb155015b246f39683fef22e7eb55
justin <jyfletcher@gmail.com>
2019-03-05 22:21:30 +0100

    Added default font.

--- init.el
+++ init.el
@@ -307,13 +307,15 @@
 	("d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" "ec5f697561eaf87b1d3b087dd28e61a2fc9860e4c862ea8e6b0b77bd4967d0ba" default)))
  '(package-selected-packages
    (quote
-	(copy-as-format web-mode org-bullets helm-swoop helm org markdown-mode restclient popwin go-autocomplete go-eldoc zenburn-theme yasnippet yaml-mode python-mode monky magit jedi go-mode flycheck erlang autopair))))
+	(copy-as-format web-mode org-bullets helm-swoop helm org markdown-mode restclient popwin go-autocomplete go-eldoc zenburn-theme yasnippet yaml-mode python-mode monky magit jedi go-mode flycheck erlang autopair)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 112 :width normal)))))
 
 ;;; Load theme
 (load-theme 'zenburn t)
