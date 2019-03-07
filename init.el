45e2ef5eb72a76c490634176c721e7e31d5390fe
justin <jyfletcher@gmail.com>
2019-03-07 22:49:34 +0100

    Changing font to a smaller pt since it was acting weird

--- init.el
+++ init.el
@@ -315,7 +315,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 112 :width normal)))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 97 :width normal)))))
 
 ;;; Load theme
 (load-theme 'zenburn t)
